4ce8bc5c1605dfa55c177416f7670d8c03524e5d
Daniel Sevitti <daniel@seriousmonkey.ca>
2025-10-29 11:33:30 -0400

    Create Message.swift

---
+++ Message.swift
@@ -0,0 +1,11 @@
+// Message.swift
+
+import Foundation
+import FirebaseFirestore
+
+struct Message: Identifiable, Codable {
+    @DocumentID var id: String?
+    let senderId: String
+    let text: String
+    let timestamp: Timestamp
+}
